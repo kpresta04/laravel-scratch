8d633b9ffee23f8917c2af05deaff1de76e57f69
Kellen Presta <kpresta04@gmail.com>
2023-08-13 16:41:31 +0100

    migrations

---
+++ database/migrations/2023_08_13_133811_create_listings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('tags', 100);
+            $table->string('company', 100);
+            $table->string('location', 100);
+            $table->string('email', 100);
+            $table->string('website', 100);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,1 @@
+<h1>Hello Listings</h1>

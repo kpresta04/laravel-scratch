7ea42d5328f7c01c3d76293462324e53decda962
Kellen Presta <kpresta04@gmail.com>
2023-08-13 18:35:29 +0100

    listing controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing 
+
+
 Route::get('/', function () {
     return view('listings');
 });
